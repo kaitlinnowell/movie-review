37a6f01d5d6efc9bde204f7345a9672bf6ed1eb4
Paul Sherrill <pauldsherrill@yahoo.com>
2024-07-29 19:01:22 -0500

    frontend logic and styling for lists pages

---
+++ client/src/pages/RatedList.jsx
@@ -0,0 +1,145 @@
+import { useQuery, useMutation } from "@apollo/client";
+import { QUERY_ME } from "../utils/queries";
+import { UN_RATE_MOVIE } from "../utils/mutations";
+// import { ADD_MOVIE_TO_FAVORITE } from "../utils/mutations";
+import Auth from "../utils/auth";
+import Movie from '../components/Movie.jsx';
+
+const RatedList = () => {
+  const [unrateMovie] = useMutation(UN_RATE_MOVIE);
+  // const [favoriteMovie] = useMutation(ADD_MOVIE_TO_FAVORITE);
+  const { loading, data } = useQuery(QUERY_ME);
+  const userData = data?.me || {};
+
+  const handleUnrateMovie = async (movieId) => {
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
+
+    if (!token) {
+      console.error("No token found");
+      return false;
+    }
+
+    try {
+      const { data } = await unrateMovie({
+        variables: { movieId },
+      });
+
+      const updatedUser = data?.removeMovie || {};
+      console.log(updatedUser);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  // const handleFavoriteMovie = async (movieId) => {
+  //   const token = Auth.loggedIn() ? Auth.getToken() : null;
+
+  //   if (!token) {
+  //     console.error("No token found");
+  //     return false;
+  //   }
+
+  //   try {
+  //     const { data } = await favoriteMovie({
+  //       variables: { movieId },
+  //     });
+
+  //     const updatedUser = data?.favoriteMovie || {};
+  //     console.log(updatedUser);
+  //   } catch (err) {
+  //     console.error(err);
+  //   }
+  // };
+
+  if (loading) {
+    return <h2>LOADING...</h2>;
+  }
+
+  return (
+    <div>
+      <h1 className="flex justify-center text-4xl">RATED</h1>
+      <div className="flex justify-center min-h-screen p-4">
+        {userData.ratedMovies?.length > 0 ? (
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 max-w-6xl">
+            {userData.ratedMovies.map((movie) => (
+              <div key={movie.movieId}>
+                <Movie
+                  src={movie.image}
+                  alt={movie.title}
+                  overlayText={movie.rating}
+                />
+                <div className="mt-1 mb-1 flex space-x-2">
+                  <button
+                    type="button"
+                    className="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:focus:ring-yellow-900"
+                  >
+                    Favorite
+                  </button>
+                  <button
+                    onClick={() => handleUnrateMovie(movie.movieId)}
+                    type="button"
+                    className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+                  >
+                    Unrate
+                  </button>
+                </div>
+              </div>
+            ))}
+            <div className="w-full h-120 flex items-center justify-center">
+            <a
+              href="/"
+              className="pb-6"
+            >
+              <img
+                src="../src/assets/plus.png"
+                alt="Plus"
+                className="w-12 h-12"
+              />
+            </a>
+          </div>
+          </div>
+        ) : (
+          <div className="flex justify-center min-h-screen p-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 max-w-6xl">
+        <div className="flex flex-col items-center">
+          <Movie
+            src="../src/assets/poster.jpg"
+            alt="The cover for movie"
+            overlayText="No Saved Movies"
+          />
+          <div className="mt-1 mb-1 flex space-x-2">
+            <button
+              type="button"
+              className="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:focus:ring-yellow-900"
+            >
+              Favorite
+            </button>
+            <button
+              type="button"
+              className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+            >
+              Unrate
+            </button>
+          </div>
+        </div>
+        <div className="w-full h-120 flex items-center justify-center">
+        <a
+              href="/"
+              className="pb-6"
+            >
+              <img
+                src="../src/assets/plus.png"
+                alt="Plus"
+                className="w-12 h-12"
+              />
+            </a>
+          </div>
+      </div>
+    </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default RatedList;
